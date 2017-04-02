f2f8f745077e07324affbb59b3afefc31ac1b067
caravelea <caraveleastefan@gmail.com>
2017-04-02 15:36:35 +0300

    Update 02.04.2017
    Am adaugat o fereastra care se va deschide cand se apasa pe butonu raportare CNAS

---
+++ RaportareCNAS.java
@@ -0,0 +1,23 @@
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class RaportareCNAS 
+{
+	public RaportareCNAS() {
+		JFrame frame = new JFrame();
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frame.setVisible(true);
+		panel.setLayout(null);
+		frame.setBounds(100, 100, 860, 660);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+	}
+	
+	
+	}
+	
